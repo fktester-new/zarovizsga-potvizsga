91e9dcac6e20f25bb43c527bc90afad1a1efea7e
fktester <kofe16@gmail.com>
2021-03-22 17:55:24 +0100

    People

--- src/main/java/hu/nive/ujratervezes/zarovizsga/people/People.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/people/People.java
@@ -1,4 +1,31 @@
 package hu.nive.ujratervezes.zarovizsga.people;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class People {
+
+   public int getNumberOfMales(String path){
+        return readFilw(path);
+   }
+
+   private int readFilw(String fileName){
+
+       try (BufferedReader reader = Files.newBufferedReader(Path.of(fileName))) {
+           String line;
+           int numberOfMales = 0;
+           while ((line = reader.readLine())  != null) {
+               String[] parts = line.split(",");
+               if (parts[4].equals("Male")){
+                   numberOfMales++;
+               }
+           }
+           return numberOfMales;
+       } catch (IOException ioe) {
+           throw new IllegalStateException("Can not read file", ioe);
+       }
+   }
 }
